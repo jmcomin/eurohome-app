53c52fe31a370f32faa43576fab7f7521caf9665
jmcomin <jmcomin@gmail.com>
2026-03-08 21:53:13 +0100

    fix: remove output standalone from next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: 'standalone',
+  // output: 'standalone', removed to allow 'npm start' (next start) to work directly in Docker
 };
 
 export default nextConfig;
